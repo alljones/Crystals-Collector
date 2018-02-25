84a37bbb5c85ef5b72e697bc6fef43aff66e762e
Allan Jones <iamallanjones@gmail.com>
2018-02-24 20:12:39 -0600

    Updated the winLoss and reset funct.

--- assets/js/game.js
+++ assets/js/game.js
@@ -8,6 +8,18 @@
     - 4. Varable for  Wins set to 0 to start;
     - 5. Variable for Losses set to 0 to start;
     - 6. Variable for display areas (goal, wins, losses, totalscore)
+
+- - - - - GAME PLAY FLOW - - - - - - - - - -  - -
+    1. Generate Random CPU Goal
+    2. On Click of Button, it will add to the userScore total
+    3. If userScore equals cpuGoal
+             - You Win
+             -Increment Wins plus 1
+        Else
+            -You Lose
+            -Increment Losses plus 1
+        
+    4. Reset back to deflaut userScore, Generate new number for buttons, startGame over again
 */
 
 
@@ -44,27 +56,32 @@ function winLoss(){
     if (userScore > cpuGoal) {
         alert("Sorry, you lost, try again!");
         losses++;
+        lossesStore.text(losses);
         reset();
         startGame();
     } else if (userScore === cpuGoal) {
         alert("Wooohooo! You WON!!!");
         wins++;
+        winsStore.text(wins);
         reset();
         startGame();
     }
 }
 
-//Reset
+// Reset
 function reset(){
-    userScore = 0;
-    redCrystal = getRandomRange(1, 12);
-    greenCrystal = getRandomRange(1, 12);
-    yellowCrystal = getRandomRange(1, 12);
-    blueCrystal = getRandomRange(1, 12);
+    userScore = 0;                          // Set user back to 0
+    totalScore.text(userScore);             // Display user on screen to O
+    redCrystal = getRandomRange(1, 12);     // Set New Random Number to crystals
+    greenCrystal = getRandomRange(1, 12);   // "
+    yellowCrystal = getRandomRange(1, 12);  // "
+    blueCrystal = getRandomRange(1, 12);    // "
+    cpuGoal = getRandomRange(19, 120);      // Set New Random Number to CPU
+    cpuStore.text(cpuGoal);                 // Display brand new CPU  on screen
 }
 
 function updateScore(val){
-    console.log(val);
+    //console.log(val);
     userScore += val;
     totalScore.text(userScore);
     winLoss();
@@ -92,16 +109,3 @@ function startGame(){
 }
 
 startGame();
-
-/*1. Generate Random CPU Goal
-    2. On Click of Button, it will add to the userScore total
-    3. If userScore equals cpuGoal
-             - You Win
-             -Increment Wins plus 1
-        Else
-            -You Lose
-            -Increment Losses plus 1
-        
-    4. Reset back to deflaut userScore, Generate new number for buttons, startGame over again
-    
-*/
